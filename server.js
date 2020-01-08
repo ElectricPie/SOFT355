e1953546fd41a82be13ec61a3cc40d8f9575bff1
Connor Priest <Kon_Priest@Hotmail.com>
2020-01-08 14:19:23 +0000

    Set up socket.io on Server and Main Menu

--- server.js
+++ server.js
@@ -1,19 +1,24 @@
-var express = require("express");
-var app = express();
+//Node packages
+var app = require("express")();
+
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
 
 var path = require('path');
 
-var port = 9000;
-
-
+//Custom packages
 var Lobby = require('./Lobby');
 
+
+var port = 9000;
 var lobbies = [];
+
 
 //Page requests
 app.get('/', function(request, response) {
     response.sendFile(path.join(__dirname + '/webPages/mainMenu.html'));
 });
+
 
 app.get('/joinPage', function(request, response) {
     response.sendFile(path.join(__dirname + '/webPages/joinGame.html'));
@@ -24,6 +29,14 @@ app.get('/lobby', function(request, response) {
 });
 
 
-app.listen(port, function() {
-    console.log("Listening on " + port); 
+io.on('connection', function(socket){
+    console.log('a user connected');
+    socket.on('disconnect', function(){
+        console.log('user disconnected');
+    });
+});
+
+
+http.listen(port, function() {
+    console.log("Listening on: " + port);
 });
