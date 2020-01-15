8f3c63635938fb749e59ec829b9cf9615ec8f22b
Connor Priest <Kon_Priest@Hotmail.com>
2020-01-15 00:36:00 +0000

    Added User Registration
    When the user connects to the server for the first time their socket is stored in the server. When the user closes the connection the server searches the stored sockets and removes the disconnected one.

--- Lobby.js
+++ Lobby.js
@@ -1,11 +1,37 @@
 class Lobby {
     constructor(host) {
         this.host = host;
+        this.players = [];
+        this.lobbyCode = "";
     }
 
     getHost() {
         return this.host;
     }
+
+    getPlayers() {
+        return this.players;
+    }
+
+    lobbyCode() {
+        return lobbyCode;
+    }
 }
 
-module.exports = Lobby;
+class Player {
+    constructor(playerName, socket) {
+        this.playerName = playerName;
+        this.socket = socket;
+    }
+
+    getName() {
+        return this.playerName;
+    }
+
+    getSocket() {
+        return this.socket;
+    }
+}
+
+module.exports.Player = Player;
+module.exports.Lobby = Lobby;
--- server.js
+++ server.js
@@ -9,14 +9,14 @@ var path = require('path');
 //Custom packages
 var Lobby = require('./Lobby');
 
-
 var port = 9000;
 var lobbies = [];
+var players = [];
 
 //Temp lobbies
-lobbies.push(new Lobby("Test Lobby 1"));
-lobbies.push(new Lobby("Test Lobby 2"));
-lobbies.push(new Lobby("Test Lobby 3"));
+lobbies.push(new Lobby.Lobby("Test Lobby 1"));
+lobbies.push(new Lobby.Lobby("Test Lobby 2"));
+lobbies.push(new Lobby.Lobby("Test Lobby 3"));
 
 
 //Page requests
@@ -40,8 +40,19 @@ const lobbyListSocket = io.of('/lobbies');
 lobbyListSocket.on('connection', function(socket){
   console.log('user connected to lobbies');
 
-  socket.on('disconnect', function(){ 
-    console.log('user disconnected from lobbies');
+  socket.on('disconnect', function(){    
+    //Looks through the list of players and removes the socket that has disconected
+    for (let i = 0; i < players.length; i++) {
+      if (players[i].getSocket() == socket) {
+        players.socket.splice(i, 1);
+      }
+    }
+  });
+
+  socket.on('register user', function (msg) { 
+    var newPlayer = new Lobby.Player(msg.name, socket);
+
+    players.push(newPlayer);   
   });
 });
 
